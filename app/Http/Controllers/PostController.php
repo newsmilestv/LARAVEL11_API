3345c740e61a2439c9312949ee38e0da0778fb03
Lee Che In First Comp <li_che_in@mail.ru>
2025-03-05 12:27:22 +0300

    app/Http/Controllers/PostController: add code public function store(Request $request): JsonResponse

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -17,9 +20,28 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|min:3|max:255',
+            'body'  => 'required|string|min:5',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status'  => false,
+                'message' => 'All fields are required',
+                'errors'  => $validator->errors()
+            ], 422);
+
+            $post = Post::create($request->all());
+
+            return response()->json([
+                'status'  => true,
+                'message' => 'Message created successfully',
+                'data'    => $post
+            ], 201);
+        }
     }
 
     /**
